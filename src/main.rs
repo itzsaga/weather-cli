66a177aed54dae33ba18331fe2d8a7bcabb395bf
Seth <sethalexander@pm.me>
2021-08-20 10:31:30 -0500

    consume args

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,13 @@
+use std::env::var;
+use std::env::args;
+
 fn main() {
-    let api_token = std::env::var("API_TOKEN")
+    let api_token = var("API_TOKEN")
         .expect("expected there to be an api token");
-    dbg!(api_token);
+
+    let mut arg_iterator = args();
+    arg_iterator.next();
+    let args: String = arg_iterator.collect();
+
+    dbg!(args);
 }
