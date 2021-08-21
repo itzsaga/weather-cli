a5db6b21853c3184b6bed4875f50cf4f1103b804
Seth <sethalexander@pm.me>
2021-08-20 21:44:39 -0500

    make a request

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use std::env::var;
+use reqwest::blocking::Client;
 use std::env::args;
+use std::env::var;
 
 fn main() {
     let api_token = var("API_TOKEN")
@@ -9,5 +10,15 @@ fn main() {
     arg_iterator.next();
     let args: String = arg_iterator.collect();
 
-    dbg!(args);
+    let client = Client::new();
+
+    let response = client
+        .get("https://api.waqi.info/search/")
+        .query(&[("token", api_token), ("keyword", args)])
+        .send()
+        .expect("a successful request")
+        .json::<serde_json::Value>()
+        .expect("expected the body to be json");
+
+    dbg!(response);
 }
